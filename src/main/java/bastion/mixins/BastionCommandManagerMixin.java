ae9d1248a5d5e64bffc8072dc8b567005c4956ec
Kahzerx <kahzer123@gmail.com>
2020-09-05 04:19:29 +0200

    oops

--- src/main/java/bastion/mixins/BastionCommandManagerMixin.java
+++ src/main/java/bastion/mixins/BastionCommandManagerMixin.java
@@ -3,12 +3,14 @@ package bastion.mixins;
 import bastion.Bastion;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.SeedCommand;
 import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CommandManager.class)
@@ -18,5 +20,10 @@ public class BastionCommandManagerMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
     private void registerCommands(CommandManager.RegistrationEnvironment environment, CallbackInfo ci){
         Bastion.registerCommands(this.dispatcher);
+    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/command/SeedCommand;register(Lcom/mojang/brigadier/CommandDispatcher;Z)V"))
+    public void onRegister(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated){
+        SeedCommand.register(dispatcher, false);
     }
 }
